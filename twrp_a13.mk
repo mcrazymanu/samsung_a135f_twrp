5e57ef1ab30c992ee8985e4630e1ab0a6508149e
mcrazymanu <mcrazymanu@gmail.com>
2022-09-04 00:29:30 +0530

    twrp_a13.mk: added charger_res_images

---
+++ twrp_a13.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from a13 device
+$(call inherit-product, device/samsung/a13/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a13/recovery/root,recovery/root)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a13
+PRODUCT_NAME := twrp_a13
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A135F
+PRODUCT_MANUFACTURER := samsung
