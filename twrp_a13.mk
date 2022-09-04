889864fcc6787d8a9c0349ca3cde089218bf17f5
mcrazymanu <mcrazymanu@gmail.com>
2022-09-04 09:06:31 +0530

    A135xxU1AVF1

--- twrp_a13.mk
+++ twrp_a13.mk
@@ -1,18 +1,11 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from a13 device
-$(call inherit-product, device/samsung/a13/device.mk)
-
-# Inherit some common twrp stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a13/device.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
